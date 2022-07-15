7d87aa72bd2ef4277f949c6f2738bd46529d1d89
lsepu <55538804+lsepu@users.noreply.github.com>
2022-07-14 22:01:32 -0500

    ADD: Loading snipper

--- src/components/ListOfPokemon.tsx
+++ src/components/ListOfPokemon.tsx
@@ -1,18 +1,36 @@
-import { Row, Col } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import { Row, Col, Spinner } from "react-bootstrap";
 import { useSelector } from "react-redux";
 import { stateType } from "../state/store";
 import PokemonCard from "./PokemonCard";
 
 const ListOfPokemon = () => {
   const { pokemons } = useSelector((state: stateType) => state.pokemon);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (pokemons.length > 1) {
+      setLoading(false);
+    }
+  }, [pokemons]);
 
   return (
     <Row className="justify-content-md-center">
-      {pokemons.map((pokemon, index) => (
-        <Col  key={index} sm style={{ display: "flex", justifyContent: "center" }}>
-          <PokemonCard pokemon={pokemon} />
-        </Col>
-      ))}
+      {!loading ? (
+        pokemons.map((pokemon, index) => (
+          <Col
+            key={index}
+            sm
+            style={{ display: "flex", justifyContent: "center" }}
+          >
+            <PokemonCard pokemon={pokemon} />
+          </Col>
+        ))
+      ) : (
+        <Spinner animation="border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
+      )}
     </Row>
   );
 };
